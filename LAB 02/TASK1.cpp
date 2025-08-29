d485bcc1f62261a456dc30d2a50ea3867591e8f6
Manahil Abbas <manahilabbas645@gmail.com>
2025-08-29 11:54:18 +0500

    Add files via upload

---
+++ LAB 02/TASK1.cpp
@@ -0,0 +1,40 @@
+// Implement a program that dynamically allocates a 1D array of integers.
+// => The size should be given by the user.
+// => Initialize all elements with zero.
+// => Then, let the user update values at different positions.
+// => Finally, print the array and deallocate memory properly.
+#include <iostream>
+using namespace std;
+int main() {
+    int *arr;
+    int size;
+    cout <<"enter the size of array"<<endl;
+    cin>>size;
+    arr=new int[size];
+    for (int i=0;i<size;i++){
+        arr[i]=0;
+    }
+    
+    cout<<"do you want to update any number \n y for yes \n n for no"<<endl;
+    char choice ;
+    cin>>choice; 
+    do{
+      if(choice=='y'){
+        cout<<"enter the position you eant to update"<<endl;
+        int index;
+        cin>> index;
+        cout<<"enter the value"<<endl;
+        int value;
+        cin>>value;
+        arr[index-1]=value;
+    }
+cout<<"do you want to update any other number \n y for yes \n n for no"<<endl;
+cin>>choice; }
+while(choice=='y');
+    for (int i=0;i<size;i++){
+        cout<<i+1<<". "<<arr[i]<<endl;
+    }
+
+
+    return 0;
+}
